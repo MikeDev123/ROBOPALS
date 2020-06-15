1a66de932d1e76acf1282f7199a35749f71ed87e
MikeDev123 <mikecalunsod@outlook.com>
2020-06-15 11:07:06 -0700

    Updates

---
+++ precache-manifest.ecfa54110d6aedcb9a7e976a26dda20b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "37de1c1aaf6a6e9bc9669f19b0f524b4",
+    "url": "/MikeDev123/ROBOPALS/index.html"
+  },
+  {
+    "revision": "c016c2724503137269b8",
+    "url": "/MikeDev123/ROBOPALS/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "d509cea3b050ce63a195",
+    "url": "/MikeDev123/ROBOPALS/static/css/main.ba8c3583.chunk.css"
+  },
+  {
+    "revision": "c016c2724503137269b8",
+    "url": "/MikeDev123/ROBOPALS/static/js/2.88b08eb1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/MikeDev123/ROBOPALS/static/js/2.88b08eb1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d509cea3b050ce63a195",
+    "url": "/MikeDev123/ROBOPALS/static/js/main.a5360b54.chunk.js"
+  },
+  {
+    "revision": "fa24f0606820b9ec519c",
+    "url": "/MikeDev123/ROBOPALS/static/js/runtime-main.6a1ad35d.js"
+  },
+  {
+    "revision": "92c3810f48104a0fa26cbef3ad302807",
+    "url": "/MikeDev123/ROBOPALS/static/media/electrical.92c3810f.woff"
+  }
+]);
